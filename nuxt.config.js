1d42b537107b3cf6315fbf3ddba61eab43c5845e
Alesia Sviridova <lvschlee@gmail.com>
2022-01-14 14:49:18 +0100

    feature(migrate-vue3): mirgate from nuxt 2 -> nuxt 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,6 @@
-export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+import { defineNuxtConfig } from 'nuxt3'
 
-  // Global App headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'My Nuxt Application',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'This is an awesome description of my Nuxt app',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: []
-};
+})
