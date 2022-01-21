395229822d0f8a1548a0d678601532d7bbddd40a
Ruslan Lukoyanov <64499427+rLukoyanov@users.noreply.github.com>
2022-01-21 21:29:03 +0300

    feature(navigation): add navigation layout (#3)
    * feature(navigation): add navigation layout
    
    * feature(navigation): connect bootstrap
    
    * feature(navigation): add new line in index.vue
    
    * feature(navigation): connect bootstrap without CDN
    
    * feature(navigation): testing lib
    
    * feature(navigation): testing lib
    
    * feature(navigation): add lib in public folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,13 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
-})
+  meta: {
+    script: [
+      {
+        src: "/bootstrap.min.js",
+      },
+    ],
+  },
+  css: ["bootstrap/dist/css/bootstrap.min.css"],
+});
